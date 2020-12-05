6b619c09ad92b4f7173671028e67d30585c044cc
Clément van Staen <clement.vanstaen@gmail.com>
2020-12-05 21:53:59 +0100

    Initialise React App

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://avatars0.githubusercontent.com/u/12551446" className="App-logo" alt="logo" />
+        <p>
+          Songbook
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
