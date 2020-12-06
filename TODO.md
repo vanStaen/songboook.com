0719afb578506296ce443b5d5a2445e720d89b31
Clément van Staen <clement.vanstaen@gmail.com>
2020-12-06 18:39:12 +0100

    drawer feature wip

--- TODO.md
+++ TODO.md
@@ -2,9 +2,15 @@
 
 ## To-dos:
 
-- [ ] Display entries
+- [ ] Show tags and lyrics
+    - [ ] Create Drawer as separate component
+    - [ ] Fetch lyrics from https://apiseeds.com/documentation/lyrics
+    - [ ] Create Tag omponent
+- [ ] Edit Tag feature
+- [ ] Edit title feature
 
 ## Completed ✓
 
 - [x] Create Rest API
 - [x] Create React App
+- [x] Display entries
--- frontend/src/component/Page/Page.js
+++ frontend/src/component/Page/Page.js
@@ -1,13 +1,24 @@
-import React from "react";
+import { React, useState } from "react";
+import { Tooltip, Drawer, Divider } from 'antd';
+import { MenuUnfoldOutlined, } from '@ant-design/icons';
 
 import './Page.css'
 
 const Page = (props) => {
+    const [visible, setVisible] = useState(false);
 
-    let title = props.page.title.replace('-', '/').replace(/ /g, '');
-    const howLongIsLong = 25;
+    const showDrawer = () => {
+        setVisible(true);
+    };
+    const onClose = () => {
+        setVisible(false);
+    };
+
+    const title = props.page.title.replace('-', '/').replace(/ /g, '');
+    const howLongIsLong = 24;
     const isLongTitle = title.length > howLongIsLong;
-    title = isLongTitle ? `${title.slice(0, howLongIsLong)}...` : title;
+    const titlePage = isLongTitle ? `${title.slice(0, howLongIsLong)}...` : title;
+    const titleDrawer = props.page.title.split('-')[1];
 
 
     return (
@@ -15,7 +26,36 @@ const Page = (props) => {
             <a href={props.page.link} target="_blank">
                 <img src={props.page.picurl} className="Page__artwork"></img>
             </a>
-            <div className="Page__title">{title}</div>
+            <div className="Page__title">{titlePage}</div>
+            <Tooltip title="Show text">
+                <div className="Page__actionicon">
+                    <MenuUnfoldOutlined onClick={showDrawer} />
+                </div>
+            </Tooltip>
+            <Drawer
+                title={titleDrawer}
+                placement="right"
+                closable={false}
+                onClose={onClose}
+                visible={visible}
+                className="Page__drawer"
+                width="350"
+            >
+                <img src={props.page.picurl} className="Page-drawer__artwork"></img>
+                <Divider orientation="left" plain>
+                    <span className="Page-drawer__diviser">
+                        Tags
+                    </span>
+                </Divider>
+                <p>{props.page.tags}</p>
+                <Divider orientation="left" plain>
+                    <span className="Page-drawer__diviser">
+                        Lyrics
+                    </span>
+                </Divider>
+                <p>Lyrics</p>
+
+            </Drawer>
         </div>
     );
 }
