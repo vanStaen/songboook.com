1d8b37108fc4ab153daa3fb3f70efc0b38e85962
Clément van Staen <clement.vanstaen@gmail.com>
2021-01-23 21:36:04 +0100

    WIP search bar

--- frontend/src/component/Search/Search.js
+++ frontend/src/component/Search/Search.js
@@ -1,13 +1,23 @@
 import { React } from 'react';
+import { Input } from 'antd';
 
 import './Search.css';
 
-const Search = () => {
+const { Search } = Input;
+
+const SearchField = () => {
+
+    const onSearch = value => console.log(value);
+
     return (
-        <div className="search__searchbar">
-            search bar
+        <div className="search__container">
+            <Search
+                placeholder="input search text"
+                onSearch={onSearch}
+                className="search__bar"
+            />
         </div>
     );
 }
 
-export default Search;
+export default SearchField;
