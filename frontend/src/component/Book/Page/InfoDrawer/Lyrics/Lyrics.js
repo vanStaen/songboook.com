414a14dab0f843bcf99cdfe9ccc5405826fb3ea0
Clément van Staen <clement.vanstaen@gmail.com>
2021-02-02 18:14:18 +0100

    stop laoding lyrics when there is some

--- frontend/src/component/Book/Page/InfoDrawer/Lyrics/Lyrics.js
+++ frontend/src/component/Book/Page/InfoDrawer/Lyrics/Lyrics.js
@@ -27,35 +27,36 @@ const Lyrics = (props) => {
     const loadLyrics = () => {
         if (lyrics !== "Loading ..." && lyrics !== null) {             
             console.log("One should not pursue things one already has.")
-            return 
-         }
-        async function fetchLyrics() {
-            const response = await axios({
-                url: process.env.REACT_APP_API_URL + `lyrics/${props.id}`,
-                method: "GET",
-            });
-            if ((response.status !== 200) & (response.status !== 201)) {
-                throw new Error("Error!");
-            }
-            const result = await response.data;
-            const resultLyrics = result;
-            return resultLyrics;
-        }
-        // fetch Entries
-        fetchLyrics()
-            .then((resData) => {
-                //console.log(resData.lyrics);
-                if (resData.lyrics.length > 0) {
-                    setLyrics(fixLatinEncoding(resData.lyrics));
-                    setFound(true);
-                } else {
-                    setFound(false);
+         } else {
+            async function fetchLyrics() {
+                const response = await axios({
+                    url: process.env.REACT_APP_API_URL + `lyrics/${props.id}`,
+                    method: "GET",
+                });
+                if ((response.status !== 200) & (response.status !== 201)) {
+                    throw new Error("Error!");
                 }
-            })
-            .catch(error => {
-                console.log(error);
-                setFound(false);
-            });
+                const result = await response.data;
+                const resultLyrics = result;
+                return resultLyrics;
+            }
+            // fetch Entries
+            fetchLyrics()
+                .then((resData) => {
+                    //console.log(resData.lyrics);
+                    if (resData.lyrics.length > 0) {
+                        setLyrics(fixLatinEncoding(resData.lyrics));
+                        setFound(true);
+                    } else {
+                        setFound(false);
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                    setFound(false);
+                });
+         }
+
     };
 
     const handleRetryFetchLyrics = () => {
