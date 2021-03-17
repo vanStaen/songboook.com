c1e6343e56e23d6869c95619844436d0124bb1f8
vanStaen <clement.vanstaen@gmail.com>
2021-03-17 18:50:13 +0100

    randomized endpoint done

--- api/randomized.js
+++ api/randomized.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const getfirstResultGoogleSearch = require('../helpers/getfirstResultGoogleSearch');
 const { Client } = require("pg");
 
 
@@ -21,12 +20,19 @@ client.connect(err => {
 router.get("/", async (req, res) => {
   try {
     const randomized = await client.query(`SELECT * FROM songbook WHERE randomized=false`);
-    if (randomized.rows.length > 0) {
-      res.status(201).json(songbook.rows);
+    if (randomized.rows.length === 0) {
+      await setAllRandomizedToFalse();
+      const randomizedNew = await client.query(`SELECT * FROM songbook WHERE randomized=false`);
+      const rowChosen = getRandomNumber(randomizedNew.rows.length - 1);
+      await setSongToRandomizedTrue(randomizedNew.rows[rowChosen].id);
+      res.status(201).json(randomizedNew.rows[rowChosen]);
+    } else if (randomized.rows.length === 1) {
+      await setAllRandomizedToFalse();
+      res.status(201).json(randomized.rows[0]);
     } else {
-      res.status(400).json({
-        error: `No data found with id#${req.params.id}`,
-      });
+      const rowChosen = getRandomNumber(randomized.rows.length - 1);
+      await setSongToRandomizedTrue(randomized.rows[rowChosen].id);
+      res.status(201).json(randomized.rows[rowChosen]);
     }
   } catch (err) {
     res.status(400).json({
@@ -39,12 +45,19 @@ router.get("/", async (req, res) => {
 router.get("/:checked", async (req, res) => {
   try {
     const randomized = await client.query(`SELECT * FROM songbook WHERE checked=${req.params.checked} AND randomized=false`);
-    if (randomized.rows.length > 0) {
-      res.status(201).json(songbook.rows);
+    if (randomized.rows.length === 0) {
+      await setAllRandomizedToFalse();
+      const randomizedNew = await client.query(`SELECT * FROM songbook WHERE checked=${req.params.checked} AND randomized=false`);
+      const rowChosen = getRandomNumber(randomizedNew.rows.length - 1);
+      await setSongToRandomizedTrue(randomizedNew.rows[rowChosen].id);
+      res.status(201).json(randomizedNew.rows[rowChosen]);
+    } else if (randomized.rows.length === 1) {
+      await setAllRandomizedToFalse();
+      res.status(201).json(randomized.rows[0]);
     } else {
-      res.status(400).json({
-        error: `No data found with id#${req.params.id}`,
-      });
+      const rowChosen = getRandomNumber(randomized.rows.length - 1);
+      await setSongToRandomizedTrue(randomized.rows[rowChosen].id);
+      res.status(201).json(randomized.rows[rowChosen]);
     }
   } catch (err) {
     res.status(400).json({
@@ -52,6 +65,32 @@ router.get("/:checked", async (req, res) => {
     });
   }
 });
+
+
+// Function to set back all randomise to false
+const setAllRandomizedToFalse = async () => {
+  try {
+    await client.query(`UPDATE songbook SET randomized=false`);
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+// Set Song as already randomized (=have been the result of the randomizer already)
+const setSongToRandomizedTrue = async (id) => {
+  try {
+    await client.query(`UPDATE songbook SET randomized=true WHERE id=${id}`);
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+const getRandomNumber = (max) => {
+  const random = Math.floor(Math.random() * (max + 1));
+  return random;
+}
 
 module.exports = router;
 
