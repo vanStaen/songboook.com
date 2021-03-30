89471700dcea2dcfd5d5b495744eb98050a189a4
vanStaen <clement.vanstaen@gmail.com>
2021-03-30 14:13:31 +0200

    loader spin on random icon

--- frontend/src/component/Menu/Random/Random.jsx
+++ frontend/src/component/Menu/Random/Random.jsx
@@ -1,11 +1,18 @@
-import Dice from "../../../images/dice.svg";
-import { getRandomized } from "./getRandomized";
+import { useState } from "react";
 import { Tooltip } from "antd";
+import { SyncOutlined } from '@ant-design/icons';
+
+
+import { getRandomized } from "./getRandomized";
+import Dice from "../../../images/dice.svg";
 
 import "./Random.css";
 
 export const Random = (props) => {
+  const [isLoading, setIsloading] = useState(false);
+
   const handleRandomPick = async () => {
+    setIsloading(true);
     let option = undefined;
     if (props.onlyFlagKnown === 1) {
       option = false;
@@ -13,18 +20,19 @@ export const Random = (props) => {
     if (props.onlyFlagKnown === 2) {
       option = true;
     }
-
     try {
       const randomPage = await getRandomized(option);
       props.setRandomPageId(randomPage.id);
     } catch (error) {
       console.log(error);
     }
+    setIsloading(false);
   };
 
   return (
     <Tooltip placement="left" title="Random song">
       <div className="Random__float" onClick={handleRandomPick}>
+        {isLoading && <SyncOutlined spin className="Random__spin" style={{ color: "#6E0F1C" }} />}
         <img className="Random__dice" src={Dice} alt="logo" />
       </div>
     </Tooltip>
