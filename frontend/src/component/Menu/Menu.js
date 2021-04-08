0bb2fa2086d715513f34431e5d9a9a4f8d512cb0
Clément van Staen <clement.vanstaen@gmail.com>
2021-04-08 12:10:28 +0200

    display menu

--- frontend/src/component/Menu/Menu.js
+++ frontend/src/component/Menu/Menu.js
@@ -1,5 +1,4 @@
 import { React, useState } from "react";
-import { QuestionOutlined, CheckOutlined } from "@ant-design/icons";
 import { observer } from "mobx-react";
 
 import { AddSongButton } from "./AddSongButton/AddSongButton";
@@ -11,57 +10,11 @@ import { Random } from "./Random/Random";
 import { InfoButton } from "./InfoButton/InfoButton"
 import { authStore } from "../../stores/authStore";
 
-import piano from "./../../images/piano.png";
-import bass from "./../../images/bass.png";
-import guitar from "./../../images/guitar.png";
-import bookmark from "../../images/bookmark.png";
-
 import "./Menu.css";
 
 const Menu = observer((props) => {
   const [showSearch, setShowSearch] = useState(false);
   const [showSettings, setShowSettings] = useState(false);
-
-  // 0: all, 1: only unknown, 2: only known
-  const classNameFlagKnown = () => {
-    if (props.onlyFlagKnown === 0) {
-      return "menu__check inactive white";
-    } else if (props.onlyFlagKnown === 1) {
-      return "menu__check active neonwhite";
-    } else if (props.onlyFlagKnown === 2) {
-      return "menu__check active green";
-    }
-  };
-
-  const toolTipFlagKnow = () => {
-    if (props.onlyFlagKnown === 0) {
-      return null;
-    } else if (props.onlyFlagKnown === 1) {
-      return "Showing unknown only";
-    } else if (props.onlyFlagKnown === 2) {
-      return "Showing known only";
-    }
-  };
-
-  const iconFlagKnown = () => {
-    if (props.onlyFlagKnown === 0) {
-      return <CheckOutlined />;
-    } else if (props.onlyFlagKnown === 1) {
-      return <QuestionOutlined />;
-    } else if (props.onlyFlagKnown === 2) {
-      return <CheckOutlined />;
-    }
-  };
-
-  const handlerFlagKnown = () => {
-    if (props.onlyFlagKnown === 0) {
-      props.setOnlyFlagKnown(1);
-    } else if (props.onlyFlagKnown === 1) {
-      props.setOnlyFlagKnown(2);
-    } else if (props.onlyFlagKnown === 2) {
-      props.setOnlyFlagKnown(0);
-    }
-  };
 
   return (
     <div>
@@ -88,6 +41,16 @@ const Menu = observer((props) => {
             <SettingsButton
               showSettings={showSettings}
               setShowSettings={setShowSettings}
+              onlyFlagKnown={props.onlyFlagKnown}
+              setOnlyFlagKnown={props.setOnlyFlagKnown}
+              filterPiano={props.filterPiano}
+              filterBass={props.filterBass}
+              filterGuitar={props.filterGuitar}
+              setFilterPiano={props.setFilterPiano}
+              setFilterBass={props.setFilterBass}
+              setFilterGuitar={props.setFilterGuitar}
+              onlyBookmarked={props.onlyBookmarked}
+              setOnlyBookmarked={props.setOnlyBookmarked}
             />
             <SearchButton
               showSearch={showSearch}
@@ -107,147 +70,6 @@ const Menu = observer((props) => {
         <InfoButton/>
       </div>
 
-      {/*
-      <div className="menu__main">
-        <div
-          className="menu__element"
-          onClick={() => props.setFilterGuitar(!props.filterGuitar)}
-        >
-          <div className="menu__dymotagguitar">
-            <Tooltip
-              placement="right"
-              title={props.filterGuitar && "Guitar tabs hidden"}
-            >
-              &nbsp;Guitar&nbsp;
-            </Tooltip>
-          </div>
-          <Tooltip
-            placement="topLeft"
-            title={props.filterGuitar && "Guitar tabs hidden"}
-          >
-            <div
-              className={
-                !props.filterGuitar
-                  ? "menu__iconguitar"
-                  : "menu__iconguitar inactive"
-              }
-            >
-              <img width="27" height="27" src={guitar} alt={guitar}></img>
-            </div>
-          </Tooltip>
-          <div className="menu__dymobgguitar"></div>
-          {props.filterGuitar ? <div className="guitar__strike"></div> : ""}
-        </div>
-        <div
-          className="menu__element"
-          onClick={() => props.setFilterPiano(!props.filterPiano)}
-        >
-          <div className="menu__dymotagpiano">
-            <Tooltip
-              placement="right"
-              title={props.filterPiano && "Piano songs hidden"}
-            >
-              &nbsp;&nbsp;&nbsp;Piano&nbsp;
-            </Tooltip>
-          </div>
-          <div className="menu__dymobgpiano"></div>
-          {props.filterPiano ? <div className="piano__strike"></div> : ""}
-          <Tooltip
-            placement="top"
-            title={props.filterPiano && "Piano songs hidden"}
-          >
-            <div
-              className={
-                !props.filterPiano
-                  ? "menu__iconpiano"
-                  : "menu__iconpiano inactive"
-              }
-            >
-              <img width="37" height="37" src={piano} alt={piano}></img>
-            </div>
-          </Tooltip>
-        </div>
-        <div
-          className="menu__element"
-          onClick={() => props.setFilterBass(!props.filterBass)}
-        >
-          <div className="menu__dymotagbass">
-            <Tooltip
-              placement="right"
-              title={props.filterBass && "Bass tabs hidden"}
-            >
-              &nbsp;&nbsp;&nbsp;&nbsp;bass&nbsp;
-            </Tooltip>
-          </div>
-          <div className="menu__dymobgbass"></div>
-          {props.filterBass ? <div className="bass__strike"></div> : ""}
-          <Tooltip
-            placement="top"
-            title={props.filterBass && "Bass tabs hidden"}
-          >
-            <div
-              className={
-                !props.filterBass ? "menu__iconbass" : "menu__iconbass inactive"
-              }
-            >
-              <img width="27" height="27" src={bass} alt={bass}></img>
-            </div>
-          </Tooltip>
-        </div>
-        <div className="menu__element">
-          <div className="menu__sidebookmark">
-            <Tooltip
-              placement="right"
-              title={props.onlyBookmarked && "Showing bookmarked only"}
-            >
-              <div
-                className={
-                  props.onlyBookmarked
-                    ? "menu__favorite active"
-                    : "menu__favorite inactive"
-                }
-                onClick={() => props.setOnlyBookmarked(!props.onlyBookmarked)}
-              >
-                <img width="25" height="30" src={bookmark} alt={bookmark}></img>
-              </div>
-            </Tooltip>
-          </div>
-          <div className="menu__bottombookmark">
-            <Tooltip
-              placement="top"
-              title={props.onlyBookmarked && "Showing bookmarked only"}
-            >
-              <div
-                className={
-                  props.onlyBookmarked
-                    ? "menu__favorite active"
-                    : "menu__favorite inactive"
-                }
-                onClick={() => props.setOnlyBookmarked(!props.onlyBookmarked)}
-              >
-                <img width="25" height="30" src={bookmark} alt={bookmark}></img>
-              </div>
-            </Tooltip>
-          </div>
-        </div>
-        <div className="menu__element">
-          <div className="menu__sidecheck">
-            <Tooltip placement="bottomLeft" title={toolTipFlagKnow()}>
-              <div className={classNameFlagKnown()} onClick={handlerFlagKnown}>
-                {iconFlagKnown()}
-              </div>
-            </Tooltip>
-          </div>
-          <div className="menu__bottomcheck">
-            <Tooltip placement="topRight" title={toolTipFlagKnow()}>
-              <div className={classNameFlagKnown()} onClick={handlerFlagKnown}>
-                {iconFlagKnown()}
-              </div>
-            </Tooltip>
-          </div>
-        </div>
-      </div>
-    */}
     </div>
   );
 });
--- frontend/src/component/Menu/SettingsButton/SettingsButton.jsx
+++ frontend/src/component/Menu/SettingsButton/SettingsButton.jsx
@@ -1,35 +1,183 @@
 import { React, useEffect } from "react";
-import { EyeOutlined, AppstoreOutlined, BarsOutlined, CheckOutlined } from "@ant-design/icons";
+import {
+  EyeOutlined,
+  AppstoreOutlined,
+  BarsOutlined,
+  QuestionOutlined,
+  CheckOutlined,
+} from "@ant-design/icons";
 import { Tooltip } from "antd";
 import { observer } from "mobx-react";
+
+import piano from "./../../../images/piano.png";
+import bass from "./../../../images/bass.png";
+import guitar from "./../../../images/guitar.png";
+import bookmark from "./../../../images/bookmark.png";
 
 import "./SettingsButton.css";
 
 export const SettingsButton = observer((props) => {
+  // 0: all, 1: only unknown, 2: only known
+  const classNameFlagKnown = () => {
+    if (props.onlyFlagKnown === 0) {
+      return "SettingsButton__knowflag";
+    } else if (props.onlyFlagKnown === 1) {
+      return "SettingsButton__knowflag active red";
+    } else if (props.onlyFlagKnown === 2) {
+      return "SettingsButton__knowflag active green";
+    }
+  };
+
+  const toolTipFlagKnow = () => {
+    if (props.onlyFlagKnown === 0) {
+      return "Show only song you know";
+    } else if (props.onlyFlagKnown === 1) {
+      return "Showing unknown only";
+    } else if (props.onlyFlagKnown === 2) {
+      return "Showing known only";
+    }
+  };
+
+  const iconFlagKnown = () => {
+    if (props.onlyFlagKnown === 0) {
+      return <CheckOutlined />;
+    } else if (props.onlyFlagKnown === 1) {
+      return <QuestionOutlined />;
+    } else if (props.onlyFlagKnown === 2) {
+      return <CheckOutlined />;
+    }
+  };
+
+  const handlerFlagKnown = () => {
+    if (props.onlyFlagKnown === 0) {
+      props.setOnlyFlagKnown(1);
+    } else if (props.onlyFlagKnown === 1) {
+      props.setOnlyFlagKnown(2);
+    } else if (props.onlyFlagKnown === 2) {
+      props.setOnlyFlagKnown(0);
+    }
+  };
 
   useEffect(() => {
-    props.showSettings ?
-      setTimeout(function () { document.getElementById("SettingsButton__actionContainer").style.display = "inline-block"; }, 200) :
-      document.getElementById("SettingsButton__actionContainer").style.display = "none";
-  }, [props.showSettings])
+    props.showSettings
+      ? setTimeout(function () {
+          document.getElementById(
+            "SettingsButton__actionContainer"
+          ).style.display = "inline-block";
+        }, 200)
+      : (document.getElementById(
+          "SettingsButton__actionContainer"
+        ).style.display = "none");
+  }, [props.showSettings]);
 
   return (
     <Tooltip placement="left" title="Adjust what you see">
       <div
-        className={props.showSettings ? "SettingsButton__float SettingsButton__open" : "SettingsButton__float"}
+        className={
+          props.showSettings
+            ? "SettingsButton__float SettingsButton__open"
+            : "SettingsButton__float"
+        }
       >
-        <div className="SettingsButton__actionContainer" id="SettingsButton__actionContainer">
-          <div className="SettingsButton__action">1</div>
-          <div className="SettingsButton__action">2</div>
-          <div className="SettingsButton__action">3</div> |
-          <CheckOutlined className="SettingsButton__action" />
-          <div className="SettingsButton__action">5</div> |
-          <BarsOutlined className="SettingsButton__action" />
+        <div
+          className="SettingsButton__actionContainer"
+          id="SettingsButton__actionContainer"
+        >
+          <div
+            className={
+              props.filterPiano
+                ? "SettingsButton__action icon__piano inactive"
+                : "SettingsButton__action icon__piano"
+            }
+            onClick={() => props.setFilterPiano(!props.filterPiano)}
+          >
+            {" "}
+            <Tooltip
+              placement="bottom"
+              title={
+                props.filterPiano
+                  ? "Show all songs for piano"
+                  : "Hide all songs for piano"
+              }
+            >
+              <img width="22" height="22" src={piano}></img>
+            </Tooltip>
+          </div>
+          <div
+            className={
+              props.filterBass
+                ? "SettingsButton__action icon__bass inactive"
+                : "SettingsButton__action icon__bass"
+            }
+            onClick={() => props.setFilterBass(!props.filterBass)}
+          >
+            {" "}
+            <Tooltip
+              placement="bottom"
+              title={
+                props.filterBass
+                  ? "Show all songs for bass"
+                  : "Hide all songs for bass"
+              }
+            >
+              <img width="17" height="17" src={bass}></img>
+            </Tooltip>
+          </div>
+          <div
+            className={
+              props.filterGuitar
+                ? "SettingsButton__action icon__guitar inactive"
+                : "SettingsButton__action icon__guitar"
+            }
+            onClick={() => props.setFilterGuitar(!props.filterGuitar)}
+          >
+            <Tooltip
+              placement="bottom"
+              title={
+                props.filterGuitar
+                  ? "Show all songs for guitar"
+                  : "Hide all songs for guitar"
+              }
+            >
+              <img width="17" height="17" src={guitar}></img>
+            </Tooltip>
+          </div>
+          |
+          <Tooltip placement="bottom" title={toolTipFlagKnow()}>
+            <div
+              className={`SettingsButton__action ${classNameFlagKnown()}`}
+              onClick={handlerFlagKnown}
+            >
+              {iconFlagKnown()}
+            </div>
+          </Tooltip>
+          <div
+            className={
+              props.onlyBookmarked
+                ? "SettingsButton__action icon__bookmark"
+                : "SettingsButton__action icon__bookmark inactive"
+            }
+            onClick={() => props.setOnlyBookmarked(!props.onlyBookmarked)}
+          >
+            <Tooltip
+              placement="bottom"
+              title={
+                props.onlyBookmarked
+                  ? "Showing only bookmarked songs"
+                  : "Show only bookmarked songs"
+              }
+            >
+              <img width="17" height="18" src={bookmark}></img>
+            </Tooltip>
+          </div>
+          |
+          <BarsOutlined className="SettingsButton__action SettingsButton__padding" />
           <AppstoreOutlined className="SettingsButton__action" /> |
         </div>
         <EyeOutlined
           onClick={() => props.setShowSettings(!props.showSettings)}
-          className="SettingsButton__icon" />
+          className="SettingsButton__icon"
+        />
       </div>
     </Tooltip>
   );
