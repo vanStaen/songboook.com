3e26c8cc4e60ad33601cfcf67621d5be9afdd4af
vanStaen <clement.vanstaen@gmail.com>
2021-04-08 09:41:32 +0200

    open lock on hover

--- frontend/src/component/Menu/LoginButton/LoginButton.jsx
+++ frontend/src/component/Menu/LoginButton/LoginButton.jsx
@@ -1,16 +1,22 @@
-import { LockOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { LockOutlined, UnlockOutlined } from "@ant-design/icons";
 import { Tooltip } from "antd";
 
 import "./LoginButton.css";
 
 export const LoginButton = (props) => {
+  const [isMouseOver, setIsMouseOver] = useState(false);
   return (
     <Tooltip placement="left" title="Login">
       <div
         className={"LoginButton__float"}
         onClick={() => props.setShowLoginForm(!props.showLoginForm)}
+        onMouseEnter={() => setIsMouseOver(true)}
+        onMouseLeave={() => setIsMouseOver(false)}
       >
-        <LockOutlined className="LoginButton__icon" />
+        {isMouseOver ?
+          <UnlockOutlined className="LoginButton__icon" /> :
+          <LockOutlined className="LoginButton__icon" />}
       </div>
     </Tooltip>
   );
