40434b85728a92b6da6b5a13ab0c9b9978ad7b30
Clément van Staen <clement.vanstaen@gmail.com>
2021-04-09 17:14:56 +0200

    rework Addform WIP

--- frontend/src/component/AddForm/AddForm.js
+++ frontend/src/component/AddForm/AddForm.js
@@ -1,144 +1,153 @@
-import { React } from 'react';
-import { Modal, Form, Input, Radio, notification } from 'antd';
+import { React } from "react";
+import { Modal, Form, Input, Radio, notification } from "antd";
 import { observer } from "mobx-react";
-import axios from 'axios';
+import axios from "axios";
 
-import './AddForm.css';
+import { displayStore } from "../../stores/displayStore";
+import "./AddForm.css";
 
 const AddForm = observer((props) => {
-
-    const handleAddSong = (values) => {
-        const dataRequest = { artist: values.artist, song: values.song }
-        if (values.type === "bass") { dataRequest.bass = true; }
-        if (values.type === "piano") { dataRequest.piano = true; }
-        if (values.level) { dataRequest.level = values.level; }
-        if (values.link) { dataRequest.link = values.link; }
-        if (values.picurl) { dataRequest.picurl = values.picurl; }
-        if (values.videourl) { dataRequest.videourl = values.videourl; }
-        postAddSong(dataRequest);
-        props.setShowAddForm(false);
-    };
-
-    const postAddSong = (data) => {
-        async function postNew(data) {
-            const response = await axios({
-                url: process.env.REACT_APP_API_URL + "/songbook",
-                method: 'POST',
-                data: data,
-            });
-            if ((response.status !== 200) & (response.status !== 201)) {
-                throw new Error("Error!");
-            }
-            const patchResult = await response.data;
-            return patchResult;
-        }
-        // post new Entries
-        postNew(data).then((resData) => {
-            console.log(resData);
-            notification.success({ description: `New song successfully posted!`, });
-            props.setNewSongAdded(true);
-        }
-        ).catch(error => {
-            notification.error({ description: `Unauthorized! Please login.`, });
-            console.log("error", error.message);
-        });
+  const handleAddSong = (values) => {
+    const dataRequest = { artist: values.artist, song: values.song };
+    if (values.type === "bass") {
+      dataRequest.bass = true;
     }
+    if (values.type === "piano") {
+      dataRequest.piano = true;
+    }
+    if (values.level) {
+      dataRequest.level = values.level;
+    }
+    if (values.link) {
+      dataRequest.link = values.link;
+    }
+    if (values.picurl) {
+      dataRequest.picurl = values.picurl;
+    }
+    if (values.videourl) {
+      dataRequest.videourl = values.videourl;
+    }
+    postAddSong(dataRequest);
+    displayStore.setShowPage("book");
+  };
 
-    const handleCancel = () => {
-        props.setShowAddForm(false);
-    };
+  const postAddSong = (data) => {
+    async function postNew(data) {
+      const response = await axios({
+        url: process.env.REACT_APP_API_URL + "/songbook",
+        method: "POST",
+        data: data,
+      });
+      if ((response.status !== 200) & (response.status !== 201)) {
+        throw new Error("Error!");
+      }
+      const patchResult = await response.data;
+      return patchResult;
+    }
+    // post new Entries
+    postNew(data)
+      .then((resData) => {
+        console.log(resData);
+        notification.success({ description: `New song successfully posted!` });
+        props.setNewSongAdded(true);
+      })
+      .catch((error) => {
+        notification.error({ description: `Unauthorized! Please login.` });
+        console.log("error", error.message);
+      });
+  };
 
-    const [form] = Form.useForm();
+  const handleCancel = () => {
+    displayStore.setShowPage("book");
+  };
 
-    return (
-        <>
-            <Modal
-                visible={props.showAddForm}
-                onCancel={handleCancel}
-                className="addFormModal"
-                centered={true}
-                onOk={() => {
-                    form
-                        .validateFields()
-                        .then(values => {
-                            form.resetFields();
-                            handleAddSong(values);
-                        })
-                        .catch(info => {
-                            console.log('Validate Failed:', info);
-                        });
-                }}
-            >
-                <Form
-                    form={form}
-                    layout="vertical"
-                    name="addSongForm"
-                    initialValues={{
-                        type: 'guitar',
-                    }}
-                >
-                    <Form.Item
-                        name="artist"
-                        label="Artist, Band or Composer"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input at least the name of the artist!',
-                            },
-                        ]}
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        name="song"
-                        label="Song name"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please input at least the name of the song!',
-                            },
-                        ]}
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        name="link"
-                        label="Tabs, Notes or Chords"
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        name="picurl"
-                        label="Link to the album cover"
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        name="videourl"
-                        label="Link to the youtube video"
-                    >
-                        <Input />
-                    </Form.Item>                    
-                    <Form.Item name="type" className="collection-create-form_last-form-item">
-                        <Radio.Group>
-                            <Radio value="guitar">Guitar</Radio>
-                            <Radio value="bass">Bass</Radio>
-                            <Radio value="piano">Piano</Radio>
-                        </Radio.Group>
-                    </Form.Item>
-                    <Form.Item name="level" className="collection-create-form_last-form-item">
-                        <Radio.Group>
-                            <Radio value="easy">Easy</Radio>
-                            <Radio value="medium">Medium</Radio>
-                            <Radio value="hard">Hard</Radio>
-                        </Radio.Group>
-                    </Form.Item>
-                </Form>
+  const [form] = Form.useForm();
 
-            </Modal>
-        </>
-    );
+  return (
+    <>
+      <Modal
+        visible={true}
+        onCancel={handleCancel}
+        className="addFormModal"
+        centered={true}
+        onOk={() => {
+          form
+            .validateFields()
+            .then((values) => {
+              form.resetFields();
+              handleAddSong(values);
+            })
+            .catch((info) => {
+              console.log("Validate Failed:", info);
+            });
+        }}
+      >
+        <Form
+          form={form}
+          layout="vertical"
+          name="addSongForm"
+          initialValues={{
+            type: "guitar",
+          }}
+        >
+          <Form.Item
+            name="artist"
+            label="Artist, Band or Composer"
+            rules={[
+              {
+                required: true,
+                message: "Please input at least the name of the artist!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="song"
+            label="Song name"
+            rules={[
+              {
+                required: true,
+                message: "Please input at least the name of the song!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item name="link" label="Tabs, Notes or Chords">
+            <Input />
+          </Form.Item>
+          <Form.Item name="picurl" label="Link to the album cover">
+            <Input />
+          </Form.Item>
+          <Form.Item name="videourl" label="Link to the youtube video">
+            <Input />
+          </Form.Item>
 
-})
+          <Form.Item
+            name="type"
+            className="collection-create-form_last-form-item"
+          >
+            <Radio.Group>
+              <Radio value="guitar">Guitar</Radio>
+              <Radio value="bass">Bass</Radio>
+              <Radio value="piano">Piano</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item
+            name="level"
+            className="collection-create-form_last-form-item"
+          >
+            <Radio.Group>
+              <Radio value="easy">Easy</Radio>
+              <Radio value="medium">Medium</Radio>
+              <Radio value="hard">Hard</Radio>
+            </Radio.Group>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  );
+});
 
 export default AddForm;
