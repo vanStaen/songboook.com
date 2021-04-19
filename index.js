e336d34b81c724864174a7329f6fcbec97883c8d
vanStaen <clement.vanstaen@gmail.com>
2021-04-19 13:18:45 +0200

    certbot failed

--- index.js
+++ index.js
@@ -30,9 +30,6 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, "build", "index.html")); });
-app.get('/.well-known/acme-challenge/:content', function (req, res) {
-  res.send('k2HJjcoGkBYNQgZ4U6TpevjSzz6tOSKyn6f7RSdYE0M.Jr5r8DGVQ4AD-PDm8eKRyzFImCTHpPmykZ4DY_41uOk')
-})
 
 // Router to API endpoints
 app.use("/songbook", require("./api/songbook"));
