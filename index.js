9fe0d13affade78182807543168daeca8dfc9441
vanStaen <clement.vanstaen@gmail.com>
2022-06-02 17:58:20 +0200

    fix redirecting BE

--- index.js
+++ index.js
@@ -65,7 +65,9 @@ db.sequelize.sync().then((req) => {});
 
 // Set up for React
 app.use(express.static(path.join(__dirname, "build")));
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, "build", "index.html")); });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 // Listen on a port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
