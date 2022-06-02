76f0ec4ced638579111430c9631141509fc10192
vanStaen <clement.vanstaen@gmail.com>
2022-06-02 18:45:28 +0200

    navigate to login

--- frontend/src/pages/EmailVerified/EmailVerified.jsx
+++ frontend/src/pages/EmailVerified/EmailVerified.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { LoadingOutlined } from "@ant-design/icons";
-import { useTranslation } from "react-i18next";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 import { postEmailVerified } from "./postEmailVerified";
+import { displayStore } from "../../stores/displayStore";
 import { Book } from "../../component/Book/Book";
 
 import "./EmailVerified.css";
@@ -13,8 +13,8 @@ export const EmailVerified = () => {
   const [isVerified, setIsVerified] = useState(false);
   const [randomPageId, setRandomPageId] = useState(false);
   const [newSongAdded, setNewSongAdded] = useState(false);
-  const { t } = useTranslation();
   const params = useParams();
+  const navigate = useNavigate();
 
   const emailIsVerified = useCallback(async () => {
     const success = await postEmailVerified(params.verifyCode);
@@ -34,56 +34,58 @@ export const EmailVerified = () => {
   return (
     <>
       <div className="emailVerified__container">
-        <div className={`emailVerified__textContainer ${isVerified ? "emailVerified__success" : "emailVerified__error"}`}>
-          {
-            isLoading ? (
-              <LoadingOutlined className="emailVerified__loader" />
-            ) : isVerified ? (
-              <div className="emailVerified__text">
-                <strong>Your email have been verified</strong> <br />
+        <div
+          className={`emailVerified__textContainer ${
+            isVerified ? "emailVerified__success" : "emailVerified__error"
+          }`}
+        >
+          {isLoading ? (
+            <LoadingOutlined className="emailVerified__loader" />
+          ) : isVerified ? (
+            <div className="emailVerified__text">
+              <strong>Your email have been verified</strong> <br />
               Thank you and welcome in our community!
-                <br />
+              <br />
               You can now go ahead and log yourself in.
-                <br />
-                <div className="emailVerified__link">
-                  You will be redirected to the{" "}
-                  <span
-                    className="link"
-                    onClick={() => {
-                      document.location.href = "/";
-                    }}
-                  >
-                    {" "}
-                  login page
+              <br />
+              <div className="emailVerified__link">
+                You will be redirected to the{" "}
+                <span
+                  className="link"
+                  onClick={() => {
+                    document.location.href = "/";
+                  }}
+                >
+                  start page
                 </span>
                 .
               </div>
-              </div>
-            ) : (
-                  <div className="emailVerified__text">
-                    <strong>Your email could not be verified!</strong>
-                    <br />
-                    <br />
+            </div>
+          ) : (
+            <div className="emailVerified__text">
+              <strong>Your email could not be verified!</strong>
+              <br />
+              <br />
               Something went wrong in the verification of the email linked to
               your account!
-                    <br />
-                    <div className="emailVerified__link">
-                      What can you do? Go back to the
-                        {" "}
-                      <span
-                        className="link"
-                        onClick={() => {
-                          document.location.href = "/";
-                        }}
-                      >
-                        login page
+              <br />
+              <div className="emailVerified__link">
+                What can you do? Go back to the{" "}
+                <span
+                  className="link"
+                  onClick={() => {
+                    displayStore.setShowPage("login");
+                    navigate("/");
+                  }}
+                >
+                  login page
                 </span>
-                      {", "}
+                {", "}
                 sign in, and follow the infos on how to request a new
                 verification link.
               </div>
-                  </div>
-                )}
+            </div>
+          )}
         </div>
       </div>
 
